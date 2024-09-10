3acc3e34ccbe7578752d4c994ebabb3202be8b52
Hari Sharan Bhuju <haribhuju@users.noreply.github.com>
2024-09-10 12:39:04 +1000

    task: add counter to textarea

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 import "./style.css";
+
+const MAXWORD = 150;
+
+const textAreaEl = document.querySelector("#textarea");
+const spanCounterEL = document.querySelector(".field-counter");
+
+window.addEventListener("DOMContentLoaded", () => {
+  spanCounterEL.textContent = MAXWORD;
+});
+
+textAreaEl.addEventListener("input", () => {
+  const textAreaELValueLength = textAreaEl.value.length;
+  const charsLeft = MAXWORD - textAreaELValueLength;
+  spanCounterEL.textContent = charsLeft;
+});
