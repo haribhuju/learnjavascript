5015e0528c82b856679584834213706eba767ce0
Hari Sharan Bhuju <haribhuju@users.noreply.github.com>
2024-09-23 11:17:06 +1000

    fetch using xhr

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 import "./style.css";
+
+const pElement = document.querySelector(".app__result");
+const btnElement = document.querySelector(".app__button");
+
+function generateJokes() {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open("GET", "https://api.chucknorris.io/jokes/random");
+
+  xhr.onreadystatechange = function () {
+    if (this.readyState === 4) {
+      if (this.status === 200) {
+        const joke = JSON.parse(this.responseText).value;
+        pElement.textContent = joke;
+      } else {
+        console.log("error");
+      }
+    }
+  };
+
+  xhr.send();
+}
+
+btnElement.addEventListener("click", generateJokes);
+document.addEventListener("DOMContentLoaded", generateJokes);
